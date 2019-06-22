d23ba4f4de52c75d699fb7b4fda74ab2bcb9c9a8
Elijah Cobb <elijah@elijahcobb.com>
2019-06-21 22:36:30 -0400

    Created session.

---
+++ src/objects/Session.ts
@@ -0,0 +1,49 @@
+/**
+ *
+ * Elijah Cobb
+ * elijah@elijahcobb.com
+ * https://elijahcobb.com
+ *
+ *
+ * Copyright 2019 Elijah Cobb
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+import { ECSQLObject, ECSQLQuery } from "@elijahjcobb/nosql";
+import { User } from "./User";
+
+export interface SessionProps {
+	userId: string;
+}
+
+export class Session extends ECSQLObject<SessionProps> {
+
+	public constructor() {
+
+		super("session", {
+			userId: "string"
+		});
+
+	}
+
+	public async getUser(): Promise<User> {
+
+		if (this.props.userId === undefined) throw Error("Attempted to fetch user for session when session does not have userId.");
+		return await ECSQLQuery.getObjectWithId(User, this.props.userId);
+
+	}
+
+}
