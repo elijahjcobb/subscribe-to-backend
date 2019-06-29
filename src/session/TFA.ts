4343d262473f1319586712de0909cf3fcfbe09f2
Elijah Cobb <elijah@elijahcobb.com>
2019-06-28 21:41:16 -0400

    Added TOTP library for 2FA authentication.

---
+++ src/session/TFA.ts
@@ -0,0 +1,56 @@
+/**
+ *
+ * Elijah Cobb
+ * elijah@elijahcobb.com
+ * https://elijahcobb.com
+ *
+ *
+ * Copyright 2019 Elijah Cobb
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+import * as TOTP from "otplib";
+
+export abstract class TFA {
+
+	public static generateSecret(): string {
+
+		return TOTP.authenticator.generateSecret();
+
+	}
+
+	public static generateCode(secret: string): string {
+
+		return TOTP.authenticator.generate(secret);
+
+	}
+
+	public static verify(secret: string, code: string): boolean {
+
+		try {
+
+			return TOTP.authenticator.check(code, secret);
+
+		} catch (err) {
+
+			console.error(err);
+			return false;
+
+		}
+
+	}
+
+}
